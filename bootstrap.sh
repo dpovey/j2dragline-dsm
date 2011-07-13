86773589cd3e6b2a832d325a2b43ad602baca42e
Dean Povey <dean.povey@biznet3.com>
2011-07-14 05:40:14 +1000

    Cleaned up some redundant stuff, now how proper dependencies working.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,5 +5,13 @@ topdir() {
 J2DRAGLINE=${J2DRAGLINE:-`topdir`}
 BUILDDIR=${J2DRAGLINE}/Build
 [ -d ${BUILDDIR} ] || mkdir -p ${BUILDDIR}
-(cd ${BUILDDIR}; cmake ${J2DRAGLINE}/Sources)
-echo "Run make from ${BUILDDIR}"
+
+# Dependencies
+[ -d ${BUILDDIR}/Dependencies ] || mkdir -p ${BUILDDIR}/Dependencies
+(cd ${BUILDDIR}/Dependencies; 
+ cmake ${J2DRAGLINE}/Dependencies -DJ2DRAGLINE_BINARY_DIR=${BUILDDIR} &&
+ make)
+
+# Main
+(cd ${BUILDDIR}; cmake ${J2DRAGLINE}/Sources -DGTEST_ROOT=${BUILDDIR})
+echo "(cd ${BUILDDIR}; make)"
